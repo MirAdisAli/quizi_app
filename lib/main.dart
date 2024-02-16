5322d351a1d15c19fcf5ed290cff815669e22b28
Mir Adis Ali <93128908+MirAdisAli@users.noreply.github.com>
2024-02-16 23:48:54 +0600

    added

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'package:quizi_app/start_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
