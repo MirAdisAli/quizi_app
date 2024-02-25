a55b6008a74e8be1ddb1be8a4695e9a06e81a408
Mir Adis Ali <93128908+MirAdisAli@users.noreply.github.com>
2024-02-26 03:51:31 +0600

    added

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:quizi_app/start_screen.dart';
+import 'package:quizi_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 78, 10, 124),
-                  Color.fromARGB(255, 109, 14, 153),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
-            child: const StartScreen()
-          ),
-        ),
-      ),
-    );
+  runApp(const Quiz());
 }
